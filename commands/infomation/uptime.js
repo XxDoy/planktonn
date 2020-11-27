f29ad0af137bc4b6553e70a59e101461c976c509
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-11-27 22:01:00 +0700

    Update uptime.js

---
+++ commands/infomation/uptime.js
@@ -0,0 +1,9 @@
+const { runInContext } = require('vm');
+const ms = require('ms');
+
+module.exports = class extends Command {
+
+    async run(message) {
+        message.channel.send(`My Uptime is \`${ms(this.client.uptime, { long: true})}\``);
+    }
+}
