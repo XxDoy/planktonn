4084da781c2feac8790006ecdf4d1afbf031b460
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-26 21:49:30 +0700

    as

--- commands/infomation/serverinfo.js
+++ commands/infomation/serverinfo.js
@@ -1,112 +1,80 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "serverinfo",
+    name: "userinfo",
     category: "info",
     run: async(client, message, args) => {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+        
+        let region;
+        switch (message.guild.region) {
+            case "europe":
+                region = '🇪🇺 Europe';
+                break;
+            case "us-east":
+                region = '🇺🇸 us-east'
+                break;
+            case "us-west":
+                region = '🇺🇸 us-west';
+                break;
+            case "us-south":
+                region = '🇺🇸 us-south'
+                break;
+            case "us-central":
+                region = '🇺🇸 us-central'
+                break;
+        }
 
-    let region;
-    switch (message.guild.region) {
-        case "brazil":
-            region = 'Brazil'
-            break;
-        case "hongkong":
-            region = 'Hong Kong'
-            break;
-        case "india":
-            region = 'India'
-            break;
-        case "japan":
-            region = 'Japan'
-            break;
-        case "russia":
-            region = 'Russia'
-            break;
-        case "singapore":
-            region = 'Singapore'
-            break;
-        case "southafrica":
-            region = 'South Africa'
-            break;
-        case "sydney":
-            region = 'Sydney'
-            break;
-        case "europe":
-            region = '🇪🇺 Europe';
-            break;
-        case "us-east":
-            region = '🇺🇸 us-east'
-            break;
-        case "us-west":
-            region = '🇺🇸 us-west';
-            break;
-        case "us-south":
-            region = '🇺🇸 us-south'
-            break;
-        case "us-central":
-            region = '🇺🇸 us-central'
-            break;
+        const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
+        const members = message.guild.members.cache;
+        const channels = message.guild.channels.cache;
+        const emojis = message.guild.emojis.cache;
+
+        const embed = new MessageEmbed()
+            .setThumbnail(message.guild.iconURL({ dynamic: true }))
+            .setColor('RANDOM')
+            .setTitle(`${message.guild.name} server stats`)
+            .addFields({
+                name: "🔐 Owner: ",
+                value: message.guild.owner.user.tag,
+                inline: true
+            }, {
+                name: "👥 Members: ",
+                value: `There are ${message.guild.memberCount} users!`,
+                inline: true
+            }, {
+                name: "📈 Members Online: ",
+                value: `There are ${message.guild.members.cache.filter(m => m.user.presence.status == "online").size} users online!`,
+                inline: true
+            }, {
+                name: "💻 Total Bots: ",
+                value: `There are ${message.guild.members.cache.filter(m => m.user.bot).size} bots!`,
+                inline: true
+            }, {
+                name: "➕ Creation Date: ",
+                value: message.guild.createdAt.toLocaleDateString("en-us"),
+                inline: true
+            }, {
+                name: "📊 Roles Count: ",
+                value: `There are ${message.guild.roles.cache.size} roles in this server.`,
+                inline: true,
+            }, {
+                name: `🏴 Region: `,
+                value: region,
+                inline: true
+            }, {
+                name: `🎫 Verified: `,
+                value: message.guild.verified ? 'Server is verified' : `Server isn't verified`,
+                inline: true
+            }, {
+                name: '🚀 Boosters: ',
+                value: message.guild.premiumSubscriptionCount >= 1 ? `There are ${message.guild.premiumSubscriptionCount} Boosters` : `There are no boosters`,
+                inline: true
+            }, {
+                name: "👍 Emojis: ",
+                value: message.guild.emojis.cache.size >= 1 ? `There are ${message.guild.emojis.cache.size} emojis!` : 'There are no emojis',
+                inline: true
+            })
+        await message.channel.send(embed)
     }
 }
-}
-    const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
-    const members = message.guild.members.cache;
-    const channels = message.guild.channels.cache;
-    const emojis = message.guild.emojis.cache;
-
-
-    const embed = new MessageEmbed()
-        .setThumbnail(message.guild.iconURL({ dynamic: true }))
-        .setColor('RANDOM')
-        .setTitle(`${message.guild.name} server stats`)
-        .addFields({
-            name: "🔐 Owner: ",
-            value: message.guild.owner.user.tag,
-            inline: true
-        }, {
-            name: "👥 Members: ",
-            value: `There are ${message.guild.memberCount} users!`,
-            inline: true
-        }, {
-            name: "📈 Members Online: ",
-            value: `There are ${message.guild.members.cache.filter(m => m.user.presence.status == "online").size} users online!`,
-            inline: true
-        }, {
-            name: "💻 Total Bots: ",
-            value: `There are ${message.guild.members.cache.filter(m => m.user.bot).size} bots!`,
-            inline: true
-        }, {
-            name: "➕ Creation Date: ",
-            value: message.guild.createdAt.toLocaleDateString("en-us"),
-            inline: true
-        }, {
-            name: "📊 Roles Count: ",
-            value: `There are ${message.guild.roles.cache.size} roles in this server.`,
-            inline: true,
-        }, {
-            name: `🏴 Region: `,
-            value: region,
-            inline: true
-        }, {
-            name: `🎫 Verified: `,
-            value: message.guild.verified ? 'Server is verified' : `Server isn't verified`,
-            inline: true
-        }, {
-            name: '🚀 Boosters: ',
-            value: message.guild.premiumSubscriptionCount >= 1 ? `There are ${message.guild.premiumSubscriptionCount} Boosters` : `There are no boosters`,
-            inline: true
-        }, {
-            name: "👍 Emojis: ",
-            value: message.guild.emojis.cache.size >= 1 ? `There are ${message.guild.emojis.cache.size} emojis!` : 'There are no emojis',
-            inline: true
-        }, {
-            name: "📈 Text Channels: ",
-            value: `There are ${channels.filter(channel => channel.type === 'text').size} text channels`,
-            inline: true
-        }, {
-            name: "💻 Voice Channels: ",
-            value: `There are ${channels.filter(channel => channel.type === 'voice').size} voice channels`,
-            inline: true
-        })
-    await message.channel.send(embed)
