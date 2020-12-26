898ee12b1339aa861db1e12b9a3f4103fb78c902
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-26 21:13:28 +0700

    as

--- commands/infomation/serverinfo.js
+++ commands/infomation/serverinfo.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "userinfo",
+    name: "serverinfo",
     category: "info",
     run: async(client, message, args) => {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
