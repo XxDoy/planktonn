79e0fa79c3ec1f904521d4a475344c27af2613ac
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-28 21:20:03 +0700

    Update verify.js

--- commands/utilities/verify.js
+++ commands/utilities/verify.js
@@ -5,10 +5,9 @@ module.exports = {
     category: "utilites",
     timeout: 10000,
     run: async(client, message, args) => {
-        if (!message.guild.me.hasPermission("MANAGE_ROLES")) return message.channel.send("I require \`MANAGE_ROLES\` permission.");
-
-        const role = message.guild.roles.cache.get('793111000173248533');
-
-        await message.member.roles.add(roles.id).catch(err => console.log(err));
+        
+        message.member.addRole("793111000173248533").then(
+            message.react('✔️')
+        ).catch(err => console.log(err))
     }
 }
