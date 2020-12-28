f8c20da26a058210386864c1ea94aec734604e3c
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-28 16:32:58 +0700

    sad

--- commands/utilities/remind.js
+++ commands/utilities/remind.js
@@ -6,23 +6,22 @@ module.exports = {
     category: "utilites",
     timeout: 10000,
     run: async(client, message, args) => {
-    let timeuser = args[0]
-    let reason = args.slice(1).join("")
+    let time = args[0]
 
-    if(!timeuser) return message.reply(":x: You Should Enter Time | second(s) minute(m) hours(h) ")
-    if(!reason) return message.reply(":x: You Should Enter Reason")
+    if (!time) {
+     return message.channel.send(':x: You Should Enter Time | second(s) minute(m) hours(h) ')
+    }
+    
+    const reminder = args.join(" ").slice(args[0].lenght);
 
-    db.set(`remind.${message.author.id}`,Date.now() + ms(timeuser))
-    message.channel.send(`I will Remind You, @${message.author.username}`)
-    const interval = setInterval(function() {
+    if (reminder) {
+        if (!args[1]) {
+            return message.channel.send(`@${message.author.id}, You have set a reminder, i wil remind you ${time}`)
 
-
-        if(Date.now() > db.fetch(`remind.${message.author.id}`)){
-            db.delete(`remind.${message.author.id}`)
-            message.author.send(`**Remind:**${reason}`)
-            .catch(e => console.log(e))
-            clearInterval(interval)
+            setTimeout(function () {
+                message.member.send(`${reminder}`)
+            }, ms(time));
         }
-    },1000)
+    }
 }
 }
