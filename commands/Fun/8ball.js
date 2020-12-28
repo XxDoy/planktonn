ce44989b5870fbade7c60858c7ce391744b1ddcc
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-28 20:46:52 +0700

    Update 8ball.js

--- commands/Fun/8ball.js
+++ commands/Fun/8ball.js
@@ -7,7 +7,7 @@ module.exports = {
     run: async(client, message, args) => {
 
     if(!args[2]) return message.reply("Please Ask a Full Question!");
-    let replies = ["Yes", "No", "I dont know", "Ask Again Later", "Maybe", "Yes But Not Now", "Maybe Later", "Yes But Not Today", "Dont Tell Anyone But Yes", "Bot Always True", "Maybe tomorrow"];
+    let replies = ["Yes", "No", "I dont know", "Ask Again Later", "Maybe", "Yes But Not Now", "Maybe Later", "Yes But Not Today", "Dont Tell Anyone But Yes", "Bot Always True", "Maybe tomorrow", "tomorrow"];
 
     let result = Math.floor((Math.random() * replies.length));
     let question = args.slice(1).join(" ");
