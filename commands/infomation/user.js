1bfcae5439396d27d6bc15b61a31fd764022e70d
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-28 19:43:26 +0700

    sda

---
+++ commands/infomation/user.js
@@ -0,0 +1,66 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "user",
+    category: "info",
+    run: async(client, message, args) => {
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+
+        let status;
+        switch (user.presence.status) {
+            case "online":
+                status = "Online";
+                break;
+            case "dnd":
+                status = "Do Not Disturb";
+                break;
+            case "idle":
+                status = "Idle";
+                break;
+            case "invisible":
+                status = "Invisible";
+                break;
+        }
+
+        const embed = new MessageEmbed()
+            .setTitle(`${user.user.username} Details`)
+            .setColor("RANDOM")
+            .setThumbnail(user.user.displayAvatarURL({ dynamic: true }))
+            .addFields({
+                name: "ID: ",
+                value: user.user.id,
+            }, {
+                name: "Name: ",
+                value: user.user.username,
+                inline: true
+            }, {
+                name: "Discriminator: ",
+                value: `#${user.user.discriminator}`,
+                inline: true
+            }, {
+                name: "Current Status: ",
+                value: status,
+            }, {
+                name: "Activity: ",
+                value: user.presence.activities[0] ? user.presence.activities[0].name : `User isn't playing a game!`,
+                inline: true
+            }, {
+                name: 'Avatar link: ',
+                value: `[Click Here](${user.user.displayAvatarURL()})`,
+            }, {
+                name: 'Creation Date: ',
+                value: user.user.createdAt.toLocaleDateString("en-us"),
+                inline: true
+            }, {
+                name: 'Joined Date: ',
+                value: user.joinedAt.toLocaleDateString("en-us"),
+                inline: true
+            }, {
+                name: 'User Roles: ',
+                value: user.roles.cache.map(role => role.toString()).join(" ,"),
+                inline: true
+            })
+
+        await message.channel.send(embed)
+    }
+}
--- commands/utilities/remind.js
+++ commands/utilities/remind.js
@@ -1,29 +1,28 @@
 const Discord = require("discord.js")
 const ms = require("ms")
-
+const db = require("quick.db")
 module.exports = {
     name: "remind",
     category: "utilites",
     timeout: 10000,
     run: async(client, message, args) => {
-    let time = args[0]
+    let timeuser = args[0]
+    let reason = args.slice(1).join("")
 
-    if (!time) {
-     return message.channel.send(':x: You Should Enter Time | second(s) minute(m) hours(h) ')
-    }
-    
-    const reminder = args.join(" ").slice(args[0].lenght);
+    if(!timeuser) return message.reply(":x: You Should Enter Time | second(s) minute(m) hours(h) ")
+    if(!reason) return message.reply(":x: You Should Enter Reason")
 
-    if (reminder) {
-        if (!args[1]) {
-            return message.channel.send('You need to spesify a reminder')
+    db.set(`remind.${message.author.id}`,Date.now() + ms(timeuser))
+    message.channel.send(`I will Remind You, @${message.author.username}`)
+    const interval = setInterval(function() {
+
+
+        if(Date.now() > db.fetch(`remind.${message.author.id}`)){
+            db.delete(`remind.${message.author.id}`)
+            message.author.send(`**Remind:**${reason}`)
+            .catch(e => console.log(e))
+            clearInterval(interval)
         }
-
-        message.channel.send(`You have set a reminder, i wil remind you ${time}`)
-            
-        setTimeout(function () {
-                message.member.send(`${reminder}`)
-            }, ms(time));
-        }
-    }
+    },1000)
+}
 }
