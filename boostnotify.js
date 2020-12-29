b72c0f7cb3d3a743f4a4e016218cc23f1f10c630
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-29 10:19:36 +0700

    Update boostnotify.js

--- boostnotify.js
+++ boostnotify.js
@@ -1,6 +1,6 @@
 module.exports = async (client, oldMember, newMember) => {
     // boost
     if (!oldMember.premiumSince && newMember.premiumSince) {
-        return client.channel.cache.get("724262137358188566").send.message.channel.id !== ("793111314296864798") (`Yoo!**${newMember.user.tag}** Udah Boost Server Ini, Thank You`)
+        return client.channel.cache.get("724262137358188566").send.message.channel.id !== ("749869788985294858") (`Yoo!**${newMember.user.tag}** Udah Boost Server Ini, Thank You`)
     }
 }
