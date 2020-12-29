56158196037ba2ded6c4a755b82544d9f152b784
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-29 07:53:00 +0700

    Update verify.js

--- commands/utilities/verify.js
+++ commands/utilities/verify.js
@@ -5,12 +5,12 @@ module.exports = {
     category: "utilites",
     cooldown: 20,
     run: async(client, message, args) => {
-        if (message.channel.id !== "") {
+        if (message.channel.id !== "793111314296864798") {
             return;
         }
 
     await message.delete();
-    await message.member.roles.add("");
+    await message.member.roles.add("793111000173248533");
     return;
     }
 }
