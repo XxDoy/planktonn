06198cb708dbf2ac02574573409e6632b90ab966
XxDoy <69180906+XxDoy@users.noreply.github.com>
2020-12-30 10:07:04 +0700

    Update translate.js

--- commands/utilities/translate.js
+++ commands/utilities/translate.js
@@ -3,7 +3,7 @@ const translt = require('translate-google')
 module.exports= {
     name : 'translate',
     run : async(client, message, args) => {
-        translt(args.join(" "), {to : 'ind'}).then(res =>{
+        translt(args.join(" "), {to : 'en'}).then(res =>{
             message.channel.send(res)
         }).catch(err => {
             message.channel.send('An error has occured')
