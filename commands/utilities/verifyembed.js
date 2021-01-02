d355f48f0065d6f903acdfb9d39641950f78c39b
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-02 18:33:47 +0700

    Update verifyembed.js

--- commands/utilities/verifyembed.js
+++ commands/utilities/verifyembed.js
@@ -6,10 +6,12 @@ module.exports = {
     timeout: 10000,
     run: async(client, message, args) => {
     
+    const botIcon = new client.user.displayAvatarURL();
     const kiss = new MessageEmbed()
         .setColor('Black')
         .setTitle('How To Access All Channel?')
         .setDescription('***Type : b?verify***')
+        .setFooter(`${botIcon} | ${client.user.username}`)
     return message.channel.send(kiss)
 
 }
