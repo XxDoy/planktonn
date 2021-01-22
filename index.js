ac1b066deed4cbde42fa96a80986be4892318136
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-22 15:42:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ client.on("ready", () => {
             .setColor("RANDOM")
             .setDescription(`Welcome **${member.displayName}** To Server **${member.guild.name}**\n\n<a:verified:755234123974574120> Jangan Lupa Untuk Terlebih Dahulu Membaca <#723202845989535877> Dan <#668495345235525653>\n\n <a:verified:755234123974574120> Take Role Di <#703552850281758740> dan <#724269570780561520>\n\n <a:verified:755234123974574120> Biar Kenal Intro Donk Di <#705323788732203069> \n\n Semoga Betah Di Server ${member.guild.name}`)
             .setImage('https://media.discordapp.net/attachments/753168518979256342/795916542633574400/ezgif.com-add-text.gif')
-            .setFooter(`Kamu Member Ke ${member.guild.memberCount}`, message.guild.iconURL())
+            .setFooter(`Kamu Member Ke ${member.guild.memberCount}`)
         Channel.send(embed)
     })
 // jangan pernah naruh command atau apapun dibawah ini karena script ini menentukan antar file jadi wajib diatas biar berfungsi
