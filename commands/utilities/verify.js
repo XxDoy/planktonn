380778ecb26b11dd3761303a5bd064e0e92ddaf9
XxDoy <69180906+XxDoy@users.noreply.github.com>
2021-01-22 14:46:38 +0700

    Update verify.js

--- commands/utilities/verify.js
+++ commands/utilities/verify.js
@@ -1,16 +1,30 @@
-const Discord = require("discord.js");
+const { Discord, MessageEmbed } = require("discord.js");
 
 module.exports = {
     name: "verify",
     category: "utilites",
     cooldown: 20,
     run: async(client, message, args) => {
+
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+
+        const avatar = message.author.displayAvatarURL({ size: 4096, dynamic: true });
+
         if (message.channel.id !== "794142711094837300") {
             return;
         }
 
     await message.member.roles.add("668491452233547816");
-    await message.delete();
+
+    let embed = new MessageEmbed()
+            .setColor('RANDOM')
+            .setTitle(`${message.author.username}`)
+            .setDescription('<a:checklist:801435724636880936>  ||  **Successfully Verified!!**\n\n```EXAMPLE : h?verify```')
+            .setTimestamp()
+            .setFooter(`${message.author.username}`, avatar)
+        await message.channel.send(embed)
+        message.delete()
     return;
+    
     }
 }
